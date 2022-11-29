29085f36f6341f8ff712e1bbebbc561db6a56ea8
Andressa Da Costa <andressadacostan@gmail.com>
2022-11-28 22:15:00 -0300

    complete cycle

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -51,6 +51,7 @@ interface Cycle {
 	minutesAmount: number;
 	startDate: Date;
 	interruptedDate?: Date;
+	finishedDate?: Date;
 }
 
 export function Home() {
@@ -68,20 +69,40 @@ export function Home() {
 
 	const activeCycle = cycles.find((cycle) => cycle.id === activeCycleId);
 
+	const totalSeconds = activeCycle ? activeCycle.minutesAmount * 60 : 0;
+
 	useEffect(() => {
 		let interval: number;
 
 		if (activeCycle) {
 			interval = setInterval(() => {
-				setAmountSecondsPassed(
-					differenceInSeconds(new Date(), activeCycle.startDate)
+				const secondsDifference = differenceInSeconds(
+					new Date(),
+					activeCycle.startDate
 				);
-			}, 1000)
+
+				if (secondsDifference >= totalSeconds) {
+					setCycles((state) =>
+						state.map((cycle) => {
+							if (cycle.id === activeCycleId) {
+								return { ...cycle, finishedDate: new Date() };
+							} else {
+								return cycle;
+							}
+						})
+					);
+
+					setAmountSecondsPassed(totalSeconds);
+					clearInterval(interval);
+				} else {
+					setAmountSecondsPassed(secondsDifference);
+				}
+			}, 1000);
 		}
 		return () => {
-			clearInterval(interval)
-		}
-	}, [activeCycle])
+			clearInterval(interval);
+		};
+	}, [activeCycle, totalSeconds, activeCycleId]);
 
 	function handleCreateNewCycle(data: NewCycleFormData) {
 		// console.log(data);
@@ -102,8 +123,8 @@ export function Home() {
 	}
 
 	function handleInterruptCycle() {
-		setCycles(
-			cycles.map((cycle) => {
+		setCycles((state) =>
+			state.map((cycle) => {
 				if (cycle.id === activeCycleId) {
 					return { ...cycle, interruptedDate: new Date() };
 				} else {
@@ -114,7 +135,6 @@ export function Home() {
 		setActiveCycleId(null);
 	}
 
-	const totalSeconds = activeCycle ? activeCycle.minutesAmount * 60 : 0;
 	const currentSeconds = activeCycle ? totalSeconds - amountSecondsPassed : 0;
 
 	const minutesAmount = Math.floor(currentSeconds / 60);
@@ -134,7 +154,7 @@ export function Home() {
 	const task = watch('task');
 	const isSubmitDisable = !task;
 
-	console.log(cycles);
+	// console.log(cycles);
 
 	return (
 		<HomeContainer>
