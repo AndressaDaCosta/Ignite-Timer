d5a73176ec51855634d6572b1edeabd6e6a64559
Andressa Da Costa <andressadacostan@gmail.com>
2022-11-29 12:59:55 -0300

    Contexto no formulário

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,17 +1,16 @@
 import { HandPalm, Play } from 'phosphor-react';
-import { useForm } from 'react-hook-form';
+import { FormProvider, useForm } from 'react-hook-form';
 import { zodResolver } from '@hookform/resolvers/zod';
 import * as zod from 'zod';
-import { createContext, useEffect, useState } from 'react';
-import { differenceInSeconds } from 'date-fns';
+import { createContext, useState } from 'react';
 
 import {
 	HomeContainer,
 	StartCountdownButton,
 	StopCountdownButton
 } from './styles';
-import { NewCycleForm } from './components/NewCycleForm';
-import { Countdown } from './components/Countdown';
+import { NewCycleForm } from './Components/NewCycleForm';
+import { Countdown } from './Components/Countdown';
 
 interface Cycle {
 	id: string;
@@ -25,30 +24,43 @@ interface Cycle {
 interface CyclesContextType {
 	activeCycle: Cycle | undefined;
 	activeCycleId: string | null;
+	amountSecondsPassed: number;
 	markCurrentCycleAsFinished: () => void;
+	setSecondsPassed: (seconds: number) => void;
 }
 
 export const CyclesContext = createContext({} as CyclesContextType);
 
-// entendendo sobre controlled / uncontrolled
+const newCycleFormValidationSchema = zod.object({
+	task: zod.string().min(1, 'Informe a tarefa'),
+	minutesAmount: zod
+		.number()
+		.min(5, 'O ciclo precisa ser de no mínimo 5 minutos.')
+		.max(60, 'O ciclo precisa ser de no máximo 60 minutos.')
+});
 
-/* function handleSubmit(event) {
-		event.target.task.value;
-	}
-
-	<form onSubmit={handleSubmit}> 
-
-	const [task, setTask] = useState('')
-
-	function resetForm() {
-		setTask('');
-	} */
+type NewCycleFormData = zod.infer<typeof newCycleFormValidationSchema>;
 
 export function Home() {
 	const [cycles, setCycles] = useState<Cycle[]>([]);
 	const [activeCycleId, setActiveCycleId] = useState<string | null>(null);
+	const [amountSecondsPassed, setAmountSecondsPassed] = useState(0);
+
+	const newCycleForm = useForm<NewCycleFormData>({
+		resolver: zodResolver(newCycleFormValidationSchema),
+		defaultValues: {
+			task: '',
+			minutesAmount: 0
+		}
+	});
+
+	const { handleSubmit, watch, reset } = newCycleForm;
 
 	const activeCycle = cycles.find((cycle) => cycle.id === activeCycleId);
+
+	function setSecondsPassed(seconds: number) {
+		setAmountSecondsPassed(seconds);
+	}
 
 	function markCurrentCycleAsFinished() {
 		setCycles((state) =>
@@ -62,22 +74,22 @@ export function Home() {
 		);
 	}
 
-	// function handleCreateNewCycle(data: NewCycleFormData) {
-	//   const id = String(new Date().getTime())
+	function handleCreateNewCycle(data: NewCycleFormData) {
+		const id = String(new Date().getTime());
 
-	//   const newCycle: Cycle = {
-	//     id,
-	//     task: data.task,
-	//     minutesAmount: data.minutesAmount,
-	//     startDate: new Date(),
-	//   }
+		const newCycle: Cycle = {
+			id,
+			task: data.task,
+			minutesAmount: data.minutesAmount,
+			startDate: new Date()
+		};
 
-	//   setCycles((state) => [...state, newCycle])
-	//   setActiveCycleId(id)
-	//   setAmountSecondsPassed(0)
+		setCycles((state) => [...state, newCycle]);
+		setActiveCycleId(id);
+		setAmountSecondsPassed(0);
 
-	//   reset()
-	// }
+		reset();
+	}
 
 	function handleInterruptCycle() {
 		setCycles((state) =>
@@ -92,26 +104,23 @@ export function Home() {
 		setActiveCycleId(null);
 	}
 
-	// const task = watch('task')
-	// const isSubmitDisable = !task
-
-	// console.log(cycles);
-
-	/**
-	 *  Prop Drilling -> QUando a gente tem MUITAS propriedades APENAS para comunicação entre componentes.
-	 * Context API -> Permite compartilhar informações entre VÁRIOS componentes ao mesmo tempo.
-	 */
+	const task = watch('task');
+	const isSubmitDisable = !task;
 
 	return (
 		<HomeContainer>
-			<form /* onSubmit={handleSubmit(handleCreateNewCycle)} */>
+			<form onSubmit={handleSubmit(handleCreateNewCycle)}>
 				<CyclesContext.Provider
 					value={{
 						activeCycle,
 						activeCycleId,
-						markCurrentCycleAsFinished
+						markCurrentCycleAsFinished,
+						amountSecondsPassed,
+						setSecondsPassed
 					}}>
-					{/* <NewCycleForm /> */}
+					<FormProvider {...newCycleForm}>
+						<NewCycleForm />
+					</FormProvider>
 					<Countdown />
 				</CyclesContext.Provider>
 
@@ -124,7 +133,8 @@ export function Home() {
 					</StopCountdownButton>
 				) : (
 					<StartCountdownButton
-						/* disabled={isSubmitDisable} */ type="submit">
+						disabled={isSubmitDisable}
+						type="submit">
 						<Play size={24} />
 						Começar
 					</StartCountdownButton>
@@ -133,3 +143,26 @@ export function Home() {
 		</HomeContainer>
 	);
 }
+// function handleCreateNewCycle(data: NewCycleFormData) {
+//   const id = String(new Date().getTime())
+
+//   const newCycle: Cycle = {
+//     id,
+//     task: data.task,
+//     minutesAmount: data.minutesAmount,
+//     startDate: new Date(),
+//   }
+
+//   setCycles((state) => [...state, newCycle])
+//   setActiveCycleId(id)
+//   setAmountSecondsPassed(0)
+
+//   reset()
+// }
+
+// console.log(cycles);
+
+/**
+ *  Prop Drilling -> QUando a gente tem MUITAS propriedades APENAS para comunicação entre componentes.
+ * Context API -> Permite compartilhar informações entre VÁRIOS componentes ao mesmo tempo.
+ */
